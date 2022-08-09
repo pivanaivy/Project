1dab9d01a1d759ab66b4ad438e0394375bb210fa
pivanaivy <106169920+pivanaivy@users.noreply.github.com>
2022-08-09 14:28:01 +0300

    Update server.js

--- classsed-docker-tutorial/server.js
+++ classsed-docker-tutorial/server.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.send('Hello World Frome GIT!!!'))
+app.get('/', (req, res) => res.send('Hello World Frome GIT master!!!'))
 
 app.get('/users', async (req, res) => {
   const users = await db.select().from('users')
