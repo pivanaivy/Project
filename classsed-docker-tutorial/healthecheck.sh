ce801112faa46423aba125cfaab0dd019cb8e433
pivanaivy <asoi720603@gmail.com>
2022-08-15 16:38:19 +0000

    healthe

--- classsed-docker-tutorial/healthecheck.sh
+++ classsed-docker-tutorial/healthecheck.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Run a curl against the JAVA instance installed in a Dockerfile
 # to do a basic health check
@@ -7,25 +7,26 @@ set -x
 
 CURL_MAX_TIME=15
 ATTEMPTS=5
-SLEEP_TIME=10
+SLEEP_TIME=5
 
 for ATTEMPT in $(seq ${ATTEMPTS}); do
     echo "Attempt ${ATTEMPT} of ${ATTEMPTS}"
-    echo "Curling against the JAVA server"
+    echo "Curling against the Jenkins server"
     echo "Should expect a 200 within ${CURL_MAX_TIME} seconds"
-    STATUS_CODE=$(curl -sL -w "%{http_code}" localhost:5000 -o /dev/null \
+    STATUS_CODE=$(curl -sL -w "%{http_code}" localhost:8080 -o /dev/null \
         --max-time ${CURL_MAX_TIME})
 
     if [[ "$STATUS_CODE" == "200" ]]; then
-        echo "JAVA has come up and ready to use after ${ATTEMPT} of ${ATTEMPTS} >
+        echo "JAVA has come up and ready to use after ${ATTEMPT} of ${ATTEMPTS} attempts"
         exit 0
     else
+	git revert HEAD
         echo "JAVA did not return a correct status code yet"
         echo "Returned: $STATUS_CODE"
-        git revert HEAD
-	sleep $(SLEEP_TIME)
+        sleep ${SLEEP_TIME}
 	docker-compose up --build -d
     fi
 done
-echo "Jenkins still hasn't returned a 200 status code ${ATTEMPTS} attempts"
+
+echo "JAVA still hasn't returned a 200 status code ${ATTEMPTS} attempts"
 exit 1
